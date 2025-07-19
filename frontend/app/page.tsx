b1d9aaf7b491627285bceab653add9e5d1f50701
Siddharth Kumar <siddharth122002@gmail.com>
2025-07-19 12:33:09 +0530

    back to localhost

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -29,7 +29,7 @@ const App: React.FC = () => {
     const formData = new FormData();
     formData.append("pdff", file);
     formData.append("uniqueId", id);
-    const res = await fetch("https://rag-ngtq.onrender.com/upload", {
+    const res = await fetch("http://localhost:4000/upload", {
       method: "POST",
       body: formData,
     });
@@ -55,7 +55,7 @@ const App: React.FC = () => {
     setInput("");
     setLoading(true);
     try {
-      const res = await fetch("https://rag-ngtq.onrender.com/chat", {
+      const res = await fetch("http://localhost:4000/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
